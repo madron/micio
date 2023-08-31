9d7010e1f7e57dadb956c6845e8626c5c28c2eb0
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2023-08-31 08:40:31 +0200

    add main app

---
+++ micio.py
@@ -0,0 +1,33 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "Micius App"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    txt_number = ft.TextField(value="0", text_align="right", width=100)
+    def minus_click(e):
+        txt_number.value = str(int(txt_number.value) - 1)
+        page.update()
+
+    def plus_click(e):
+        txt_number.value = str(int(txt_number.value) + 1)
+        page.update()
+
+    page.add(
+        ft.Row(
+            [
+                ft.Text('Micius', size=30),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        ),
+        ft.Row(
+            [
+                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
+                txt_number,
+                ft.IconButton(ft.icons.ADD, on_click=plus_click),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        )
+    )
+
+ft.app(target=main)
